e921c5415c034fc47c0b28a0fba6f874767222b4
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-09-18 15:13:30 +0200

    designed user control for user details after a game is completed

---
+++ Dewey_Decimal_System/UserControlDetails.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Dewey_Decimal_System
+{
+    partial class UserControlDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnSaveScore = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbBonus = new System.Windows.Forms.TextBox();
+            this.txbScore = new System.Windows.Forms.TextBox();
+            this.txbFinalScore = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.Font = new System.Drawing.Font("Sitka Banner", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbUsername.Location = new System.Drawing.Point(193, 260);
+            this.txbUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.Size = new System.Drawing.Size(440, 38);
+            this.txbUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 225);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(178, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(603, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Congratulation ,  You completed the game!";
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(651, 357);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(151, 38);
+            this.btnPlayAgain.TabIndex = 3;
+            this.btnPlayAgain.Text = "Play Again!";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveScore
+            // 
+            this.btnSaveScore.Location = new System.Drawing.Point(651, 260);
+            this.btnSaveScore.Name = "btnSaveScore";
+            this.btnSaveScore.Size = new System.Drawing.Size(151, 38);
+            this.btnSaveScore.TabIndex = 4;
+            this.btnSaveScore.Text = "Save Score";
+            this.btnSaveScore.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Sitka Banner", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(193, 354);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(440, 38);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Want a higher score? ";
+            // 
+            // txbBonus
+            // 
+            this.txbBonus.Font = new System.Drawing.Font("Sitka Banner", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbBonus.Location = new System.Drawing.Point(517, 128);
+            this.txbBonus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbBonus.Name = "txbBonus";
+            this.txbBonus.Size = new System.Drawing.Size(64, 31);
+            this.txbBonus.TabIndex = 6;
+            // 
+            // txbScore
+            // 
+            this.txbScore.Font = new System.Drawing.Font("Sitka Banner", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbScore.Location = new System.Drawing.Point(295, 129);
+            this.txbScore.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbScore.Name = "txbScore";
+            this.txbScore.Size = new System.Drawing.Size(64, 31);
+            this.txbScore.TabIndex = 7;
+            // 
+            // txbFinalScore
+            // 
+            this.txbFinalScore.Font = new System.Drawing.Font("Sitka Banner", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbFinalScore.Location = new System.Drawing.Point(716, 129);
+            this.txbFinalScore.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbFinalScore.Name = "txbFinalScore";
+            this.txbFinalScore.Size = new System.Drawing.Size(75, 31);
+            this.txbFinalScore.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Your Score: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Bonus Points:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(600, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Final Score:";
+            // 
+            // UserControlDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbFinalScore);
+            this.Controls.Add(this.txbScore);
+            this.Controls.Add(this.txbBonus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSaveScore);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbUsername);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "UserControlDetails";
+            this.Size = new System.Drawing.Size(1004, 417);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txbUsername;
+        private Label label1;
+        private Label label2;
+        private Button btnPlayAgain;
+        private Button btnSaveScore;
+        private TextBox textBox1;
+        private TextBox txbBonus;
+        private TextBox txbScore;
+        private TextBox txbFinalScore;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Dewey_Decimal_System/UserControlDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dewey_Decimal_System
+{
+    public partial class UserControlDetails : UserControl
+    {
+        public UserControlDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
