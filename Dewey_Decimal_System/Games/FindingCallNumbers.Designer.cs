8effac258738cb01a1b4c694f233528f1275d553
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-10-30 12:53:43 +0200

    ui backend completed , ready to implement gamification

--- Dewey_Decimal_System/Games/FindingCallNumbers.Designer.cs
+++ Dewey_Decimal_System/Games/FindingCallNumbers.Designer.cs
@@ -35,55 +35,69 @@
             this.txbDescription = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lblTimer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnChoice1
             // 
+            this.btnChoice1.BackColor = System.Drawing.Color.Moccasin;
+            this.btnChoice1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
             this.btnChoice1.Location = new System.Drawing.Point(59, 311);
             this.btnChoice1.Name = "btnChoice1";
             this.btnChoice1.Size = new System.Drawing.Size(170, 139);
             this.btnChoice1.TabIndex = 0;
             this.btnChoice1.Text = "button1";
-            this.btnChoice1.UseVisualStyleBackColor = true;
+            this.btnChoice1.UseVisualStyleBackColor = false;
             this.btnChoice1.Click += new System.EventHandler(this.btnChoice1_Click);
             // 
             // btnChoice2
             // 
+            this.btnChoice2.BackColor = System.Drawing.Color.PapayaWhip;
+            this.btnChoice2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
             this.btnChoice2.Location = new System.Drawing.Point(271, 311);
             this.btnChoice2.Name = "btnChoice2";
             this.btnChoice2.Size = new System.Drawing.Size(170, 139);
             this.btnChoice2.TabIndex = 1;
             this.btnChoice2.Text = "button2";
-            this.btnChoice2.UseVisualStyleBackColor = true;
+            this.btnChoice2.UseVisualStyleBackColor = false;
             this.btnChoice2.Click += new System.EventHandler(this.btnChoice2_Click);
             // 
             // btnChoice3
             // 
+            this.btnChoice3.BackColor = System.Drawing.Color.Bisque;
+            this.btnChoice3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
             this.btnChoice3.Location = new System.Drawing.Point(484, 311);
             this.btnChoice3.Name = "btnChoice3";
             this.btnChoice3.Size = new System.Drawing.Size(170, 139);
             this.btnChoice3.TabIndex = 2;
             this.btnChoice3.Text = "button3";
-            this.btnChoice3.UseVisualStyleBackColor = true;
+            this.btnChoice3.UseVisualStyleBackColor = false;
             this.btnChoice3.Click += new System.EventHandler(this.btnChoice3_Click);
             // 
             // btnChoice4
             // 
+            this.btnChoice4.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnChoice4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
             this.btnChoice4.Location = new System.Drawing.Point(710, 311);
             this.btnChoice4.Name = "btnChoice4";
             this.btnChoice4.Size = new System.Drawing.Size(170, 139);
             this.btnChoice4.TabIndex = 3;
             this.btnChoice4.Text = "button4";
-            this.btnChoice4.UseVisualStyleBackColor = true;
+            this.btnChoice4.UseVisualStyleBackColor = false;
             this.btnChoice4.Click += new System.EventHandler(this.btnChoice4_Click);
             // 
             // txbDescription
             // 
-            this.txbDescription.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txbDescription.Location = new System.Drawing.Point(143, 180);
+            this.txbDescription.BackColor = System.Drawing.Color.White;
+            this.txbDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbDescription.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.txbDescription.Location = new System.Drawing.Point(176, 186);
             this.txbDescription.Name = "txbDescription";
             this.txbDescription.ReadOnly = true;
-            this.txbDescription.Size = new System.Drawing.Size(623, 39);
+            this.txbDescription.Size = new System.Drawing.Size(572, 40);
             this.txbDescription.TabIndex = 4;
             this.txbDescription.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -111,6 +125,50 @@
             this.lblTimer.TabIndex = 9;
             this.lblTimer.Text = "00:00";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(103, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Option 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(327, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Option 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(773, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Option 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(538, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Option 3";
+            // 
             // FindingCallNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
@@ -118,6 +176,10 @@
             this.BackgroundImage = global::Dewey_Decimal_System.Properties.Resources._1ddf1f1334afb0767824a62552d0fadb__backgrounds_free_black_backgrounds;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(955, 527);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTimer);
             this.Controls.Add(this.txbDescription);
@@ -142,5 +204,9 @@
         private TextBox txbDescription;
         private Label label1;
         private Label lblTimer;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
--- Dewey_Decimal_System/Games/FindingCallNumbers.cs
+++ Dewey_Decimal_System/Games/FindingCallNumbers.cs
@@ -8,7 +8,7 @@ namespace Dewey_Decimal_System.Games
     public partial class FindingCallNumbers : Form
     {
         private TreeGameLevel treeGameLevel;
-        private bool lvl1 = false, lvl2 = false, lvl3 = false;
+        private bool lvl1 = true, lvl2 = false, lvl3 = false;
 
         public FindingCallNumbers()
         {
@@ -17,6 +17,8 @@ namespace Dewey_Decimal_System.Games
 
         private void FindingCallNumbers_Load(object sender, EventArgs e)
         {
+            // refesh the user interface to default values
+            RefreshUI();
 
             // call method to check if files exist
             CheckFiles();
@@ -30,32 +32,28 @@ namespace Dewey_Decimal_System.Games
             // write description to the user
             txbDescription.Text = treeGameLevel.AnswerPath[2].Description;
 
-            RefreshUI();
-
-          //  btnChoice1.Text = treeGameLevel.Level1Options[0].Description; 
-         //  btnChoice2.Text = treeGameLevel.Level1Options[1].Description;   
-
-
+            // load buttons with descriptions
+            PopulateChoice(1);
         }
 
         private void btnChoice1_Click(object sender, EventArgs e)
         {
-
+            CheckAnswer(btnChoice1.Text);
         }
 
         private void btnChoice2_Click(object sender, EventArgs e)
         {
-
+            CheckAnswer(btnChoice2.Text);
         }
 
         private void btnChoice3_Click(object sender, EventArgs e)
         {
-
+            CheckAnswer(btnChoice3.Text);
         }
 
         private void btnChoice4_Click(object sender, EventArgs e)
         {
-
+            CheckAnswer(btnChoice4.Text);
         }
 
         private void CheckFiles()
@@ -83,24 +81,26 @@ namespace Dewey_Decimal_System.Games
         {
             if (i == 1)
             {
-                btnChoice1.Text = treeGameLevel.Level1Options[0].Description + " " +treeGameLevel.Level1Options[0].Number;
-                btnChoice1.Text = treeGameLevel.Level1Options[1].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[2].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[3].Description;
+                btnChoice1.Text = treeGameLevel.Level1Options[0].Description + "\n" + treeGameLevel.Level1Options[0].Number;
+                btnChoice2.Text = treeGameLevel.Level1Options[1].Description + "\n" + treeGameLevel.Level1Options[1].Number;
+                btnChoice3.Text = treeGameLevel.Level1Options[2].Description + "\n" + treeGameLevel.Level1Options[2].Number;
+                btnChoice4.Text = treeGameLevel.Level1Options[3].Description + "\n" + treeGameLevel.Level1Options[3].Number;
             }
             else if (i == 2)
             {
-                btnChoice1.Text = treeGameLevel.Level1Options[0].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[1].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[2].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[3].Description;
+                RefreshUI();
+                btnChoice1.Text = treeGameLevel.Level2Options[0].Description + "\n" + treeGameLevel.Level1Options[0].Number;
+                btnChoice2.Text = treeGameLevel.Level2Options[1].Description + "\n" + treeGameLevel.Level1Options[1].Number;
+                btnChoice3.Text = treeGameLevel.Level2Options[2].Description + "\n" + treeGameLevel.Level1Options[2].Number;
+                btnChoice4.Text = treeGameLevel.Level2Options[3].Description + "\n" + treeGameLevel.Level1Options[3].Number;
             }
             else if (i == 3)
             {
-                btnChoice1.Text = treeGameLevel.Level1Options[0].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[1].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[2].Description;
-                btnChoice1.Text = treeGameLevel.Level1Options[3].Description;
+                RefreshUI();
+                btnChoice1.Text = treeGameLevel.Level3Options[0].Description + "\n" + treeGameLevel.Level1Options[0].Number;
+                btnChoice2.Text = treeGameLevel.Level3Options[1].Description + "\n" + treeGameLevel.Level1Options[1].Number;
+                btnChoice3.Text = treeGameLevel.Level3Options[2].Description + "\n" + treeGameLevel.Level1Options[2].Number;
+                btnChoice4.Text = treeGameLevel.Level3Options[3].Description + "\n" + treeGameLevel.Level1Options[3].Number;
             }
             else
             {
@@ -109,20 +109,86 @@ namespace Dewey_Decimal_System.Games
 
         }
 
-        private void PlayGame()
-        {
-            //if (lvl1)
-            //{
-            //    btnChoice1.va
-            //}
-        }
-
         private void RefreshUI()
         {
             btnChoice1.Text = "";
             btnChoice2.Text = "";
             btnChoice3.Text = "";
             btnChoice4.Text = "";
+        }
+
+        private void CheckAnswer(String text)
+        {
+            string[] split = text.Split("\n");
+            string newSplit = split[0];
+
+            bool isAnswerCorrect;
+
+            if (lvl1)
+            {
+                isAnswerCorrect = treeGameLevel.Level1Options.Where(x => x.Description.Equals(newSplit)).Select(x => x.Correct).FirstOrDefault();
+
+                if (isAnswerCorrect)
+                {
+                    PopulateChoice(2);
+                    lvl1 = false;
+                    lvl2 = true;
+                }
+                else
+                {
+                    RefreshUI();
+                    ScoreAndDetails details = new ScoreAndDetails("You Loose");
+                    this.Hide();
+                    details.ShowDialog();
+                }
+            }
+            else if (lvl2)
+            {
+                isAnswerCorrect = treeGameLevel.Level2Options.Where(x => x.Description.Equals(newSplit)).Select(x => x.Correct).FirstOrDefault();
+                if (isAnswerCorrect)
+                {
+                    RefreshUI();
+                    PopulateChoice(3);
+                    lvl2 = false;
+                    lvl3 = true;
+                }
+                else
+                {
+                    RefreshUI();
+                    ScoreAndDetails details = new ScoreAndDetails("You Loose");
+                    this.Hide();
+                    details.ShowDialog();
+                }
+
+
+            }
+            else if (lvl3)
+            {
+                isAnswerCorrect = treeGameLevel.Level3Options.Where(x => x.Description.Equals(newSplit)).Select(x => x.Correct).FirstOrDefault();
+                if (isAnswerCorrect)
+                {
+                    //game won !!
+                    MessageBox.Show("You Win");
+                    
+                    // for next game to be played
+                    lvl1 = true;
+                    lvl2 = false;
+                    lvl3 = false;
+                    RefreshUI();
+
+                    // winner winner chicken dinner
+                    ScoreAndDetails details = new ScoreAndDetails("Congratulations, You win");
+                    this.Hide();
+                    details.ShowDialog();
+                }
+                else
+                {
+                    RefreshUI();
+                    ScoreAndDetails details = new ScoreAndDetails("You Loose");
+                    this.Hide();
+                    details.ShowDialog();
+                }
+            }
         }
     }
 }
